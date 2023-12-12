bc9478ad9b4b32e42b0a25cb623c61fa7faa284e
Coltonfr5 <Coltonfr567@gmail.com>
2023-12-12 18:43:22 -0500

    bruh

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DriveTrainSubsystem extends SubsystemBase{
 
     //public static Object set;
-    
+    //gyattt
     //4 cansparkmaxes
     private CANSparkMax frontRightMotor = new CANSparkMax(4, null);
     private CANSparkMax frontLeftMotor = new CANSparkMax(3, null);
